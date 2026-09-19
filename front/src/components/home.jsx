dbecafc5c2bddd72ccd6034c718118105a2b91c6
Fran <fperronerey@gmail.com>
2026-09-19 18:51:19 -0300

    home component base

---
+++ front/src/components/home.jsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to InnovaLab</h1>
+            <p>Explore our innovative solutions and projects.</p>
+        </div>
+    );
+}
