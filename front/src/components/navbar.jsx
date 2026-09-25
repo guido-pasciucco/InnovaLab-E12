2eee90f3a3f6369a5fca4e06dfa91c06d853273a
Fran <fperronerey@gmail.com>
2026-09-25 07:29:09 -0300

    se agregan componentes base principales

--- front/src/components/navbar.jsx
+++ front/src/components/navbar.jsx
@@ -1,16 +1,69 @@
-
 export default function Navbar() {
-    return (
-        <nav className="bg-gray-800 text-white p-4">
-            <div className="container mx-auto">
-                <div className="flex justify-between items-center">
-                    <div className="text-xl font-bold">My App</div>
-                    <div className="space-x-4">
-                        <a href="/" className="hover:text-gray-300">Home</a>
-                        <a href="/about" className="hover:text-gray-300">About</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown link
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
--- front/src/layout/layout.jsx
+++ front/src/layout/layout.jsx
@@ -1,10 +1,12 @@
 import {Outlet} from 'react-router-dom'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
+import Header from '../components/header';
 
 export default function Layout() {
     return (
         <div >
+            <Header />
             <Navbar />
             <main >
                 {/* Your main content goes here */}
